f1953e109688f161a9740e65f02b5f1f2aeb51e9
Mari Imaizumi <mariimaizumi5@gmail.com>
2025-12-11 10:16:37 +0900

    Add realloc.c

---
+++ Conquering_C_Pointers/ch4/realloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int *variable_array = NULL;
+  char buf[256];
+  int size = 0;
+  char i;
+
+  while (fgets(buf, 256, stdin) != NULL) {
+    size++
+    variable_array = realloc(variable_array, sizeof(int) * size);
+    sscanf(buf, "%d", &variable_array[size-1]);
+  }
+
+  for (i = 0; i < size; i++) {
+    printf("variable_array[%d]..%d\n", i, variable_array[i]);
+  }
+  return 0;
+}
