5b505c3b2b88cb7556daddfceea636597f63ce0f
Mari Imaizumi <mariimaizumi5@gmail.com>
2026-01-05 23:24:46 +0900

    Update lean files

--- Introduction_to_the_Lean_Language_from_Zero/LeanBook/LeanBook/EratosthenesAux.lean
+++ Introduction_to_the_Lean_Language_from_Zero/LeanBook/LeanBook/EratosthenesAux.lean
@@ -1,11 +1,16 @@
 /-このコードは動かないよ-/
+/-
+  defコマンド
+  1. 定義する関数の名前
+  2. 丸括弧で引数とその型
+  3. 返り値の型を指定
+-/
 def eratosthenesAux (n : Nat) : Array Bool := Id.run do
   let mut isPrime := Array.replicate (n + 1) true
   -- [true, true, ...]
 
   isPrime := isPrime.set! 0 false
   isPrime := isPrime.set! 1 false
-
   for p in [2 : n + 1] do
     -- p のとりうる数: 2, 3, 4, 5, 6
     if not isPrime[p]! then
--- Introduction_to_the_Lean_Language_from_Zero/LeanBook/LeanBook/NaturalNumber.lean
+++ Introduction_to_the_Lean_Language_from_Zero/LeanBook/LeanBook/NaturalNumber.lean
@@ -1,4 +1,4 @@
-/-- 自前で実装した自然数　-/
+/-- 自前で実装した自然数-/
 inductive MyNat where
   /-- ゼロ -/
   | zero
@@ -12,3 +12,9 @@ inductive MyNat where
 
 def MyNat.one := MyNat.succ .zero
 def MyNat.two := MyNat.succ .one
+
+/-- MyNat同士の足し算 -/
+def MyNat.add (m n : MyNat) : MyNat :=
+  match n with
+  | .zero => m
+  | .succ n => succ (add m n)
