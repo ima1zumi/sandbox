c934120ad18f9b79ba4ed6e2f469ed2d77bff2fa
Mari Imaizumi <mariimaizumi5@gmail.com>
2026-01-09 22:59:25 +0900

    wip

---
+++ Conquering_C_Pointers/ch4/read_slogan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SLOGAN_MAX_LEN(1024)
+
+void read_slogan(FILE *fp, char **slogan) {
+  char buf[1024];
+  int slogan_len;
+  int i;
+
+  for (i = 0;i < 7; i++) {
+    fgets(buf SLOGAN_MAX_LEN, fp);
+    slogan_len = strlen(buf);
+  }
+
+}
