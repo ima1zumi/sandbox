549d420a6e8371fb024cbd77d57c7c5a7d3e346a
Mari Imaizumi <mariimaizumi5@gmail.com>
2026-02-04 10:51:25 +0900

    LRU

---
+++ Practical_Introduction_to_SQL/LRU.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// ハッシュマップのサイズ
+// defineは前処理、コンパイル前にソースコード内の特定の文字列を指定した文字列に単純に置換する
+// マクロ
+#define MAX_KEY 1024
+
+// 双方向リストのノード
+typedef struct Node {
+  int key;
+  int value;
+  struct Node *prev; // struct Nodeへのポインタ型prev
+  struct Node *next;
+} Node;
+
+// LRUキャッシュ構造体
+typedef struct {
+
+
+} LRUCache;
+
